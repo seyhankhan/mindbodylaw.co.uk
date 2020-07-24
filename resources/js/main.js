1d5b77b86fe8bd7957347ebefa730d22f0a43ec2
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-07-24 20:54:14 +0400

    Commented out js code for now

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,8 +5,8 @@
 const scrollDownTime = 800;
 
 // how far down the page it scrolls
-scrollDistance =
-$("#scrollStopHere").offset().top - parseInt($("#scrollStopHere").css("margin-top"), 10);
+// scrolls down until #scrollStopHere's TOP is at the TOP of window
+scrollDistance = $("#scrollStopHere").offset().top - parseInt($("#scrollStopHere").css("margin-top"), 10);
 
 scrollDown = () => $("html, body").animate({
   scrollTop: scrollDistance

5e66d6285d532fe95afac3fdd60ba9fc47c69dd7
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-08-01 00:57:46 +0400

    Update googleSheetsAPI.js

--- resources/js/googleSheetsAPI.js
+++ resources/js/googleSheetsAPI.js
@@ -2,7 +2,7 @@
 
 if (window.location.href.includes("about-me")) {
   fetch("https://sheets.googleapis.com/v4/spreadsheets/11QSfvcXfhWIu_hA_BH-x-gcuWdOdATex3jhVFnDLCGs/values/About%20Me?key=AIzaSyAxFtgYFyStTch-G_kQHGI1lqKaUYg8p3Y")
-    .then(resp => resp.json()) // Transform the google sheet api url into json
+    .then(resp => resp.json()) // Transform the google sheet api html into json
     .then(data => {
       var descrip = document.getElementById("about-me-description");
       data['values'].forEach(para => descrip.innerHTML += (para.length ? para[0] : '') + '<br>');
@@ -13,7 +13,7 @@ if (window.location.href.includes("about-me")) {
 
 else if (window.location.href.includes("services-pricing")) {
   fetch("https://sheets.googleapis.com/v4/spreadsheets/11QSfvcXfhWIu_hA_BH-x-gcuWdOdATex3jhVFnDLCGs/values/Services%20Pricing?key=AIzaSyAxFtgYFyStTch-G_kQHGI1lqKaUYg8p3Y")
-    .then(resp => resp.json()) // Transform the google sheet api url into json
+    .then(resp => resp.json()) // Transform the google sheet api html into json
     .then(data => {
       text = data['values'];
       document.getElementById('title-description').innerHTML = text[0][0];
@@ -44,7 +44,7 @@ else if (window.location.href.includes("services-pricing")) {
 
 else {
   fetch("https://sheets.googleapis.com/v4/spreadsheets/11QSfvcXfhWIu_hA_BH-x-gcuWdOdATex3jhVFnDLCGs/values/Home?key=AIzaSyAxFtgYFyStTch-G_kQHGI1lqKaUYg8p3Y")
-    .then(resp => resp.json()) // Transform the google sheet api url into json
+    .then(resp => resp.json()) // Transform the google sheet api html into json
     .then(data => {
       ['tagline','about-me-info','services-pricing-info','contact-info-title','contact-info-description']
         .forEach((id, i) => document.getElementById(id).innerHTML = data['values'][i][0])
